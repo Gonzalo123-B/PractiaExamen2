c072bf7059ea544d950b7a1a75a83a2dfe845130
barboza gonzalo <barbozagonzaloagustin8@gmail.com>
2023-07-02 18:14:05 -0300

    cree un modelo de dato para las reservas

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,40 @@
 // TODO: Crear modelo de datos de Reserva
+const {DataTypes} = require("sequelize");
+const {sequelize} = require("../database")
+
+const Reserva = sequelize.define("reserva", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    surname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    fecha_salida: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    feha_llegada:{
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    codigo: {
+        type: DataTypes.STRING,
+        defaultValue: new Date(.getTime())
+    }
+},{
+    timestamps: false
+})
+
+module.exports = Reserva
